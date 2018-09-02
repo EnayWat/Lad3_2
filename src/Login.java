09ad823accfb16714e923dfdd43582912a14a655
Eanywat <59160562@go.buu.ac.th>
2018-09-03 01:48:08 +0700

    Login Commit

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton submitButton;
-    private JButton chearButton;
+    private JTextField txtUserLogin;
+    private JPasswordField pwdUserPassword;
+    private JButton btnSubmit;
+    private JButton btnChear;
+    private JLabel pwdUser;
+    private JPanel mainPanel;
+
+
+    public Login() {
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,txtUserLogin.getText()
+                +" " + new String(pwdUserPassword.getPassword()));
+            }
+        });
+        btnChear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtUserLogin.setText("");
+                pwdUserPassword.setText("");
+            }
+        });
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
 }
